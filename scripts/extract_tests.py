161305a28f18fb0e68a30e927a1050f95477fb15
Venkat Malladi <vsmalladi@gmail.com>
2025-07-18 11:30:21 -0500

    Update extract_tests.py (#9)

--- scripts/extract_tests.py
+++ scripts/extract_tests.py
@@ -121,8 +121,10 @@ def extract_tests(spec: Path, data_dir: Optional[Path], output_dir: Path, versio
     if data_dir is not None and data_dir.exists():
         dest1 = Path.cwd() / data_dir.name
         dest2 = output_dir / data_dir.name
+        dest3 = output_dir
         shutil.copytree(data_dir, dest1, symlinks=True, dirs_exist_ok=True)
         shutil.copytree(data_dir, dest2, symlinks=True, dirs_exist_ok=True)
+        shutil.copytree(data_dir, dest3, symlinks=True, dirs_exist_ok=True)
 
 
 def main():
